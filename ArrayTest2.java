ec82bb1fb98c98f5b2135ea5c636866a245dcb69
mint-crystal <73177132+mint-crystal@users.noreply.github.com>
2020-10-30 09:03:45 +0900

    201030 다차원 배열+2차원 배열의 크기

--- ArrayTest2.java
+++ ArrayTest2.java
@@ -3,54 +3,54 @@ package kh.project1.test1;
 public class ArrayTest2 {
 
 	public static void main(String[] args) {
-		//2차원 배열 선언
-		int [][] arr1;
-		int arr2 [][];
-		int [] arr3 [];
-		
-		//2차원 배열 할당
-		arr1 = new int[3][5];
-		arr2 = new int[3][]; //[][5] 오류. 1차원 배열을 선언해야함
-		arr2[0] = new int [5];
-		arr2[1] = new int [3];
-		arr2[2] = new int [10];
-		arr3 = new int[][] {{10,20,30,40,50},{10,20,30},{10,20,30,40}};
-		
-		int[][] arr = new int[2][4];
-		System.out.println(arr);	//주소 출력 : [[I@15db9742 = '[['은 주소를 저장하는 공간에 주소를 가지고 있음
-		System.out.println(arr[0]);	//주소 출력 : [I@6d06d69c
-		System.out.println(arr[1]);	//주소 출력 : [I@7852e922
-		System.out.println(arr[0][0]);
-		System.out.println(arr[0][1]);
-		System.out.println(arr[0][2]);
-		System.out.println(arr[0][3]);
-		System.out.println(arr[1][0]);
-		System.out.println(arr[1][1]);
-		System.out.println(arr[1][2]);
-		System.out.println(arr[1][3]);
-		// 위와 결과같이 같음 - for문을 사용
-		int[][] arr = new int[2][4];
-		System.out.println(arr);	
-		for(int i=0; i<arr.length; i++) {
-			System.out.println(arr[i]);
-			for(int j=0; j<arr[i].length; j++) {
-				System.out.println(arr[i][j]);
-			}
-		}
-		
-		int [][]arr = new int[2][4];
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length; j++) {
-				arr[i][j] = (j+1)*10;
-			}
-		}
-		System.out.println(arr);
-		for(int i=0; i<arr.length; i++) {
-			System.out.println(arr[i]);
-			for(int j=0; j<arr[i].length; j++) {
-				System.out.println(arr[i][j]);
-			}
-		}
+//		//2차원 배열 선언
+//		int [][] arr1;
+//		int arr2 [][];
+//		int [] arr3 [];
+//		
+//		//2차원 배열 할당
+//		arr1 = new int[3][5];
+//		arr2 = new int[3][]; //[][5] 오류. 1차원 배열을 선언해야함
+//		arr2[0] = new int [5];
+//		arr2[1] = new int [3];
+//		arr2[2] = new int [10];
+//		arr3 = new int[][] {{10,20,30,40,50},{10,20,30},{10,20,30,40}};
+//		
+//		int[][] arr = new int[2][4];
+//		System.out.println(arr);	//주소 출력 : [[I@15db9742 = '[['은 주소를 저장하는 공간에 주소를 가지고 있음
+//		System.out.println(arr[0]);	//주소 출력 : [I@6d06d69c
+//		System.out.println(arr[1]);	//주소 출력 : [I@7852e922
+//		System.out.println(arr[0][0]);
+//		System.out.println(arr[0][1]);
+//		System.out.println(arr[0][2]);
+//		System.out.println(arr[0][3]);
+//		System.out.println(arr[1][0]);
+//		System.out.println(arr[1][1]);
+//		System.out.println(arr[1][2]);
+//		System.out.println(arr[1][3]);
+//		// 위와 결과같이 같음 - for문을 사용
+//		int[][] arr = new int[2][4];
+//		System.out.println(arr);	
+//		for(int i=0; i<arr.length; i++) {
+//			System.out.println(arr[i]);
+//			for(int j=0; j<arr[i].length; j++) {
+//				System.out.println(arr[i][j]);
+//			}
+//		}
+//		
+//		int [][]arr = new int[2][4];
+//		for(int i=0; i<arr.length; i++) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				arr[i][j] = (j+1)*10;
+//			}
+//		}
+//		System.out.println(arr);
+//		for(int i=0; i<arr.length; i++) {
+//			System.out.println(arr[i]);
+//			for(int j=0; j<arr[i].length; j++) {
+//				System.out.println(arr[i][j]);
+//			}
+//		}
 	}
 
 }
